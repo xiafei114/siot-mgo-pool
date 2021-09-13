1fe6adeb38ed525475ef0e1810e6c125bae9eda3
Leon <xiafei114@gmail.com>
2021-09-13 16:50:39 +0800

    去掉beego 日志，去掉引用

--- go.mod
+++ go.mod
@@ -4,8 +4,4 @@ module github.com/xiafei114/siot-mgo-pool
 
 go 1.14
 
-require (
-	github.com/astaxie/beego v1.12.1
-	github.com/shiena/ansicolor v0.0.0-20151119151921-a422bbe96644 // indirect
-	go.mongodb.org/mongo-driver v1.6.2
-)
+require go.mongodb.org/mongo-driver v1.6.2
--- orm/orm_test.go
+++ orm/orm_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/astaxie/beego/logs"
+	"log"
 )
 
 type Logs struct {
@@ -25,14 +25,11 @@ func (m *Logs) TableName() string {
 }
 
 func init() {
-	logs.SetLogFuncCall(true)
-
 	RegisterModel(new(Logs))
 	RegisterModel(new(Logs2))
 
 	RegisterDriver("mongo", DRMongo, true)
 	RegisterDataBase("default", "mongo", "mongodb://hpyg:fire@localhost:27017/FireRobotOnline", true)
-
 }
 
 var (
@@ -47,14 +44,14 @@ func TestRead(t *testing.T) {
 	o.Using("default")
 
 	err := o.Read(&l, "UserName")
-	logs.Info(err, l)
+	log.Println(err, l)
 }
 func TestReadOrCreate(t *testing.T) {
 	o := NewOrm()
 	o.Using("default")
 
 	c, id, err := o.ReadOrCreate(&l, "UserName")
-	logs.Info(c, id, err, l)
+	log.Println(c, id, err, l)
 }
 
 func TestInsert(t *testing.T) {
@@ -62,7 +59,7 @@ func TestInsert(t *testing.T) {
 	o.Using("default")
 
 	id, err := o.Insert(&l)
-	logs.Info(id, err)
+	log.Println(id, err)
 }
 
 func TestInsertMulti(t *testing.T) {
@@ -73,7 +70,7 @@ func TestInsertMulti(t *testing.T) {
 	ls = append(ls, l)
 	ls = append(ls, l)
 	id, err := o.InsertMulti(ls)
-	logs.Info(id, err)
+	log.Println(id, err)
 }
 
 func TestUpdate(t *testing.T) {
@@ -83,7 +80,7 @@ func TestUpdate(t *testing.T) {
 	l.Ids = "5e7431f78c1b4111312cce2d"
 	l.Ltype = "group3"
 	id, err := o.Update(&l, "Ltype")
-	logs.Info(id, err, l)
+	log.Println(id, err, l)
 }
 
 func TestDelete(t *testing.T) {
@@ -93,7 +90,7 @@ func TestDelete(t *testing.T) {
 	l.Ids = "5e71816fee8b0d2ba0d24939"
 	l.Ltype = "group3"
 	cnt, err := o.Delete(&l, "Ltype")
-	logs.Info(cnt, err)
+	log.Println(cnt, err)
 }
 
 func TestQsOne(t *testing.T) {
@@ -102,7 +99,7 @@ func TestQsOne(t *testing.T) {
 
 	qs := o.QueryTable("log")
 	err := qs.Filter("username", "linleizhou1234").One(&l, "username", "type")
-	logs.Info(err, l)
+	log.Println(err, l)
 }
 
 func TestQsAll(t *testing.T) {
@@ -112,8 +109,8 @@ func TestQsAll(t *testing.T) {
 	qs := o.QueryTable("log")
 	err := qs.Filter("username__regex", "linleizhou").OrderBy("-_id", "Ltype").Limit(2, 0).All(&ls)
 	// num, err := qs.All(&ls)
-	logs.Info(err)
-	logs.Info(ls)
+	log.Println(err)
+	log.Println(ls)
 }
 
 func TestQsCount(t *testing.T) {
@@ -123,7 +120,7 @@ func TestQsCount(t *testing.T) {
 	qs := o.QueryTable("log")
 	num, err := qs.Filter("username", "linleizhou1234").Count()
 	// num, err := qs.Count()
-	logs.Info(num, err)
+	log.Println(num, err)
 }
 func TestQsUpdate(t *testing.T) {
 	o := NewOrm()
@@ -133,7 +130,7 @@ func TestQsUpdate(t *testing.T) {
 	num, err := qs.Filter("_id", "5e7431f78c1b4111312cce2d").Update(MgoSet, Params{
 		"type": "group",
 	})
-	logs.Info(num, err)
+	log.Println(num, err)
 }
 func TestQsDelete(t *testing.T) {
 	o := NewOrm()
@@ -141,7 +138,7 @@ func TestQsDelete(t *testing.T) {
 
 	qs := o.QueryTable("log")
 	num, err := qs.Filter("type", "group3").Delete()
-	logs.Info(num, err)
+	log.Println(num, err)
 }
 func TestQsIndexList(t *testing.T) {
 	o := NewOrm()
@@ -149,7 +146,7 @@ func TestQsIndexList(t *testing.T) {
 
 	qs := o.QueryTable("log")
 	indexes, err := qs.IndexView().List()
-	logs.Info(indexes, err)
+	log.Println(indexes, err)
 }
 func TestQsIndexCreateOne(t *testing.T) {
 	o := NewOrm()
@@ -161,7 +158,7 @@ func TestQsIndexCreateOne(t *testing.T) {
 	index.SetName("username").SetUnique(true)
 
 	indexes, err := qs.IndexView().CreateOne(index)
-	logs.Info(indexes, err)
+	log.Println(indexes, err)
 
 }
 func TestQsIndexDropOne(t *testing.T) {
@@ -170,13 +167,13 @@ func TestQsIndexDropOne(t *testing.T) {
 
 	qs := o.QueryTable("log")
 	err := qs.IndexView().DropOne("username")
-	logs.Info(err)
+	log.Println(err)
 }
 func TestOther(t *testing.T) {
 	// uri := "mongodb://@192.168.0.4:27017/Darwin-XYY"
 	// cs, err := connstring.Parse(uri)
-	// logs.Info(err)
-	// logs.Info(cs.Database)
-	logs.Info(time.Now().Unix())
-	logs.Info(time.Now().UTC().Unix())
+	// log.Println(err)
+	// log.Println(cs.Database)
+	log.Println(time.Now().Unix())
+	log.Println(time.Now().UTC().Unix())
 }
